196d896ed1dff0a8bc423482c3207f623b9f09bc
Rohit Chouhan <rohitchouhan07@gmail.com>
2026-09-19 22:54:03 -0500

    wip: logic for getting propert

--- src/modes.rs
+++ src/modes.rs
@@ -1,6 +1,7 @@
 pub mod inputs;
 use std::{error::Error, time::Duration};
 use rodio::{Source, microphone::MicrophoneBuilder};
+use indicatif::{ProgressBar, ProgressStyle};
 
 use crate::{RunOpts, modes};
 
@@ -19,11 +20,36 @@ pub fn main_mode(opts: RunOpts) -> Result<(), Box<dyn Error>> {
     .default_config()?
     .open_stream()?;
 
-    let recording = mic.take_duration(Duration::from_secs(opts.sample_time as u64));
+    let pb = ProgressBar::new_spinner();
+    pb.set_style(
+        ProgressStyle::default_spinner()
+            .tick_strings(&["⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏"])
+            .template("{spinner:.cyan} {msg}")
+            .unwrap(),
+    );
+
+    // NOTE: Something funny happens here, where it errors out, need to fix it
     
-    for sample in recording {
-        println!("{}", sample);
+    pb.set_message("Sampling audio...");
+    pb.enable_steady_tick(Duration::from_millis(80));
+
+    let recording = mic.take_duration(Duration::from_secs(opts.sample_time as u64));
+    let samples: Vec<f32> = recording.collect();
+
+    pb.finish_with_message("✔ Sampling complete!");
+    
+    if opts.debug == true {
+        println!("{:?}", samples);
     }
+
+    
+    // RMS (root mean square) gives you overall loudness
+    let rms = (samples.iter().map(|s| s * s).sum::<f32>() / samples.len() as f32).sqrt();
+    
+    // Peak amplitude (loudest single sample)
+    let peak = samples.iter().fold(0.0_f32, |max, &s| max.max(s.abs()));
+    
+    println!("RMS: {}, Peak: {}", rms, peak);
 
     Ok(())
 }
